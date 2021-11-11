d7ba01d17a0554c78dec5a53d152190af683adee
hitso123 <hitso.shivhare@gmail.com>
2021-11-11 11:06:51 +0000

    Account page

--- routes/main.js
+++ routes/main.js
@@ -5,4 +5,17 @@ router.get("/",(req,res) => {
 	res.render("landing");
 });
 
+router.get("/account", isLoggedIn, (req,res) => {
+	res.render("account");
+});
+
+function isLoggedIn(req, res, next){
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	else {
+		res.redirect("/login");
+	}
+}
+
 module.exports=router;
